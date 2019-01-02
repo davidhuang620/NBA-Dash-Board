34c3e1150d576e3f8f7f9820f06b835bc424b73a
David <d12yes@gmail.com>
2019-01-01 19:29:53 -0600

    adding NBA API

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,11 +17,65 @@ class App extends Component {
    }
 
 
+   // componentDidMount(){
+   //    fetch('https://jsonplaceholder.typicode.com/users')
+   //    .then(response => response.json())
+   //    .then(users => console.log(users))
+   //    // .then(users => {this.setState({nbaPlayers: users})})
+   // }
+
+
+   // unirest.get("https://api-nba-v1.p.rapidapi.com/statistics/players/playerId/{playerid}")
+   // .header("X-RapidAPI-Key", "eef1cf18famsh17f37a5a9fe4a69p1aa3d9jsn9dae8fd81353")
+   // .end(function (result) {
+   //   console.log(result.status, result.headers, result.body);
+   // });
+
+
+   // componentDidMount(){
+   //    fetch('https://data.nba.net/10s/prod/v1/2018/players.json')
+   //    .then(resp => console.log(resp))
+   //    // .then(response => response.json())
+   //    // .then(users => console.log(users))
+   //    .catch(console.log('OOPS! Error'))
+   //    // .then(users => {this.setState({nbaPlayers: users})})
+   // }
+   //
+
+
+
+
+//
+// componentDidMount(){
+//    // fetch('http://data.nba.net/prod/v1/20170201/0021600732_boxscore.json', {mode: 'no-cors'})
+//    fetch("http://stats.nba.com/stats/scoreboard/?GameDate=02/14/2015&LeagueID=00&DayOffset=0",
+//    {headers: {
+//             "Access-Control-Allow-Origin": "*"
+//             // "Content-Type": "text/plain"
+//         }}
+//    )
+//    // .then(resp => console.log(resp))
+//    // .then(response => response.json())
+//    .then(users => console.log(users))
+//    // .then(users => {this.setState({nbaPlayers: users})})
+//    .catch(console.log('OOPS! Error'))
+//    // .then(users => {this.setState({nbaPlayers: users})})
+// }
+//
+
+
    componentDidMount(){
-      fetch('https://jsonplaceholder.typicode.com/users')
+      fetch('https://data.nba.net/10s/prod/v1/2016/players.json')
+      // .then(resp => console.log(resp))
       .then(response => response.json())
-      .then(users => {this.setState({nbaPlayers: users})})
+      .then(users => console.log(users))
+      .catch(() => console.log('OOPS! Error'))
+      // .then(users => {this.setState({nbaPlayers: users})})
    }
+
+
+
+
 
 
    OnSearchChange = (event) => {
