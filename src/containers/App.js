06a6222bbb919e57ed3d6875b4c02b4d82bc7190
David <d12yes@gmail.com>
2019-01-24 23:54:15 -0600

    testing deployment

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,54 +25,14 @@ class App extends Component {
    // }
 
 
-   // unirest.get("https://api-nba-v1.p.rapidapi.com/statistics/players/playerId/{playerid}")
-   // .header("X-RapidAPI-Key", "eef1cf18famsh17f37a5a9fe4a69p1aa3d9jsn9dae8fd81353")
-   // .end(function (result) {
-   //   console.log(result.status, result.headers, result.body);
-   // });
-
-
-   // componentDidMount(){
-   //    fetch('https://data.nba.net/10s/prod/v1/2018/players.json')
-   //    .then(resp => console.log(resp))
-   //    // .then(response => response.json())
-   //    // .then(users => console.log(users))
-   //    .catch(console.log('OOPS! Error'))
-   //    // .then(users => {this.setState({nbaPlayers: users})})
-   // }
-   //
-
-
-
-
-//
-// componentDidMount(){
-//    // fetch('http://data.nba.net/prod/v1/20170201/0021600732_boxscore.json', {mode: 'no-cors'})
-//    fetch("http://stats.nba.com/stats/scoreboard/?GameDate=02/14/2015&LeagueID=00&DayOffset=0",
-//    {headers: {
-//             "Access-Control-Allow-Origin": "*"
-//             // "Content-Type": "text/plain"
-//         }}
-//    )
-//    // .then(resp => console.log(resp))
-//    // .then(response => response.json())
-//    .then(users => console.log(users))
-//    // .then(users => {this.setState({nbaPlayers: users})})
-//    .catch(console.log('OOPS! Error'))
-//    // .then(users => {this.setState({nbaPlayers: users})})
-// }
-//
 
 
    componentDidMount(){
-      fetch('https://data.nba.net/10s/prod/v1/2016/players.json')
+      fetch('https://data.nba.net/10s/prod/v1/2018/players.json')
       .then(response => response.json())
       .then(users => {this.setState({nbaPlayers: users.league.standard})})
       .catch(() => console.log('OOPS! Error'))
    }
-
-
-
 
 
 
@@ -90,7 +50,7 @@ class App extends Component {
             return (
                (obj.firstName.toLowerCase().includes(searchField.toLowerCase()) )
                ||
-               (obj.lastName.toLowerCase().includes(searchField.toLowerCase()) ) 
+               (obj.lastName.toLowerCase().includes(searchField.toLowerCase()) )
             )
          }
       )
